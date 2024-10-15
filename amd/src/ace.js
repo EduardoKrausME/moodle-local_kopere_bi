96c8a2c14859047334b3fa682f34ae23de4a6a95
Eduardo Kraus <kraus@eduardokraus.com>
2024-10-15 13:32:26 -0300

    Renamed

---
+++ amd/src/ace.js
@@ -0,0 +1,45 @@
+define(["jquery"], function($) {
+    return {
+
+        load : function(id_key, type, minLines) {
+            $.getScript("https://ajaxorg.github.io/ace-builds/src-min-noconflict/ace.js", function() {
+                var html = `
+                    <div id="editor_${id_key}_area">
+                        <div id='editor_${id_key}' style='width:100%;height:300px;'>${inputhtml}</div>
+                    </div>`;
+                var inputhtml = $(`#${id_key}`)
+                    .hide()
+                    .after(html)
+                    .val();
+                var editorDiv = $(`#editor_${id_key}`);
+
+                var editor = ace.edit(`editor_${id_key}`);
+                editor.setTheme(`ace/theme/textmate`);
+                editor.getSession().setMode(`ace/mode/${type}`);
+                editor.setValue(inputhtml);
+
+                editor.session.on("change", function(delta) {
+                    $(`#${id_key}`).val(editor.getValue());
+
+                    var numLines = editor.getSession().getDocument().getLength();
+                    var newHeight = editor.renderer.lineHeight * numLines;
+                    if (newHeight < 100) {
+                        newHeight = 100;
+                    }
+                    editorDiv.height(newHeight);
+                    editor.resize();
+                });
+
+                if (!minLines) {
+                    minLines = 6;
+                }
+
+                editor.setOptions({
+                    maxLines : 21,
+                    minLines : 6,
+                });
+            });
+        },
+
+    };
+});
