6f349514674a0ec22e17bffac99b32e70ce5b60a
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-10 14:29:53 -0300

    phpcs

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:moodle.Files.MoodleInternal.MoodleInternalGlobalState
+ * 
  * index file
  *
  * @package   local_kopere_bi
