502ca84b6201a325c2132847dcdba5732ea84e51
Eduardo Kraus <kraus@eduardokraus.com>
2025-02-14 16:51:31 -0300

    Dialog to ModalFactory

--- amd/src/filter_user.js
+++ amd/src/filter_user.js
@@ -21,7 +21,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define(["jquery", "jqueryui", "local_kopere_dashboard/dataTables_init"], function($, $ui, dataTables_init) {
+define(["jquery", "jqueryui", "core/modal_factory", "local_kopere_dashboard/dataTables_init"],
+    function($, $ui, ModalFactory, dataTables_init) {
     return {
         init : function() {
             var data_user_block_select = false;
@@ -29,60 +30,56 @@ define(["jquery", "jqueryui", "local_kopere_dashboard/dataTables_init"], functio
             var chartUser = $("#chart-user-button-open");
             chartUser.show().click(function() {
                 if (data_user_block_select) {
-                    $("#chart-user-datatable-select").dialog("open");
+                    data_user_block_select.show();
                 } else {
-                    data_user_block_select = $("#chart-user-datatable-select").dialog({
-                        resizable : false,
-                        height    : "auto",
-                        width     : "auto",
-                        maxWidth  : 400,
-                        modal     : true,
-                        classes   : {
-                            "ui-dialog" : "kopere-dashboard-modal"
-                        },
-                        show      : {
-                            effect   : "blind",
-                            duration : 200
-                        },
-                        hide      : {
-                            duration : 300
-                        },
-                        buttons   : [
-                            {
-                                text  : chartUser.attr("data-strclose"),
-                                click : function() {
-                                    $(this).dialog("close");
-                                }
-                            }
-                        ]
-                    });
 
-                    dataTables_init.init("datatable_user_select", {
-                        autoWidth    : false,
-                        columns      : [
-                            {data : "id"},
-                            {data : "fullname"},
-                            {data : "username"},
-                            {data : "email"},
-                        ],
-                        columnDefs   : [
-                            {
-                                render  : "numberRenderer",
-                                targets : 0,
-                            }
-                        ],
-                        export_title : false,
-                        order        : [[1, "asc"]],
-                        processing   : true,
-                        serverSide   : true,
-                        ajax         : {
-                            url      : chartUser.attr("data-urlajax"),
-                            type     : "POST",
-                            dataType : "json",
+                    ModalFactory.create({
+                        type: ModalFactory.types.DEFAULT,
+                        title: $("#chart-user-datatable-select").attr("title"),
+                        body: $("#chart-user-datatable-select").html(),
+                    }).then(function(modal) {
+                        if (!modal.root) {
+                            modal.root = modal._root;
                         }
-                    });
 
-                    dataTables_init.click("datatable_user_select", ["id"], chartUser.attr("data-urlclick"));
+                        data_user_block_select = modal;
+                        data_user_block_select.show();
+                        data_user_block_select.root.addClass("kopere-bi");
+                        data_user_block_select.root.find(".modal-dialog")
+                            .addClass("modal-xl").addClass("kopere-dashboard-modal");
+
+                        var urlajax = $("#chart-user-datatable-select").attr("data-urlajax");
+                        var urlclick = $("#chart-user-datatable-select").attr("data-urlclick");
+
+                        $("#chart-user-datatable-select").remove();
+
+                        dataTables_init.init("datatable_user_select", {
+                            autoWidth    : false,
+                            columns      : [
+                                {data : "id"},
+                                {data : "fullname"},
+                                {data : "username"},
+                                {data : "email"},
+                            ],
+                            columnDefs   : [
+                                {
+                                    render  : "numberRenderer",
+                                    targets : 0,
+                                }
+                            ],
+                            export_title : false,
+                            order        : [[1, "asc"]],
+                            processing   : true,
+                            serverSide   : true,
+                            ajax         : {
+                                url      : urlajax,
+                                type     : "POST",
+                                dataType : "json",
+                            }
+                        });
+
+                        dataTables_init.click("datatable_user_select", ["id"], urlclick);
+                    });
                 }
             });
         }
