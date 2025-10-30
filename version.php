7d50cde9297c157f9b993cb75886834b1b2adba8
Eduardo Kraus <kraus@eduardokraus.com>
2025-10-30 10:25:48 -0300

    V 2.6.4

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2025103000;
-$plugin->release = "2.6.3";
+$plugin->release = "2.6.4";
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_kopere_bi";
