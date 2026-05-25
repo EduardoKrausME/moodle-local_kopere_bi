c68bbda6ca37e931fb7445194ef43e5cb43a8895
Eduardo Kraus <kraus@eduardokraus.com>
2026-05-24 22:58:06 -0300

    V 3.0.0

--- version.php
+++ version.php
@@ -18,18 +18,18 @@
  * version file
  *
  * @package   local_kopere_bi
- * @copyright 2025 Eduardo Kraus {@link https://eduardokraus.com}
+ * @copyright 2026 Eduardo Kraus {@link https://eduardokraus.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026022800;
-$plugin->release = "2.7.2";
+$plugin->version = 2026052400;
+$plugin->release = "3.0.0";
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_kopere_bi";
 
 $plugin->dependencies = [
-    "local_kopere_dashboard" => 2026011400,
+    "local_kopere_dashboard" => 2026052400,
 ];
