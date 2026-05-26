015eb3b535741a0a6036a192fc2372fe2d21fc11
Eduardo Kraus <kraus@eduardokraus.com>
2026-05-26 08:38:20 -0300

    Moodle boilerplate not found

--- lang/pt_br/local_kopere_bi.php
+++ lang/pt_br/local_kopere_bi.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * lang pt_br file
@@ -689,7 +703,7 @@ $string['report_field_windows_xp_title'] = 'Windows xp';
 $string['report_field_workshop_title'] = 'Workshop';
 $string['report_field_wrong_answers_title'] = 'Respostas erradas';
 $string['report_new'] = 'Novo relatório para "{$a}"';
-$string['report_page_001_block_1_element_2_title'] = 'List de online estudantes';
+$string['report_page_001_block_1_element_2_title'] = 'Lista de online estudantes';
 $string['report_page_001_block_2_element_1_title'] = 'Curso acesso tempo';
 $string['report_page_001_block_3_element_1_title'] = 'Navegadores com the most acesso tempo';
 $string['report_page_001_block_3_element_2_title'] = 'Operating systems com the most acesso tempo';
