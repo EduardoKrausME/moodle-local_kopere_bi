71a2968d90839c41eae66896ba7f9aa919930433
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-11 13:37:31 -0300

    V 3.1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026071400;
-$plugin->release = "3.0.4";
+$plugin->version = 2026081100;
+$plugin->release = "3.1.0";
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_kopere_bi";
