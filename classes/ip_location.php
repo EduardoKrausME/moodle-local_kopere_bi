12d2ae3a7335c87b03f08e5a3ba1fe096591cc04
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-24 11:44:24 -0300

    Code checker

--- classes/ip_location.php
+++ classes/ip_location.php
@@ -24,8 +24,6 @@
 
 namespace local_kopere_bi;
 
-defined('MOODLE_INTERNAL') || die;
-
 use dml_exception;
 use stdClass;
 
--- classes/task/ip_location_update.php
+++ classes/task/ip_location_update.php
@@ -5,6 +5,14 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Scheduled IP location refresh.
@@ -15,8 +23,6 @@
  */
 
 namespace local_kopere_bi\task;
-
-defined('MOODLE_INTERNAL') || die;
 
 use local_kopere_bi\ip_location;
 
